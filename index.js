16e0c93149befba8d916dbb4467139a6d3df516c
leon-do <leondo123@gmail.com>
2021-08-28 11:42:23 -0400

    reorg

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
-const getFlow = require("./getFlow");
-const startFlow = require("./startFlow");
-const stopFlow = require("./stopFlow");
+const getFlow = require("./src/getFlow");
+const startFlow = require("./src/startFlow");
+const stopFlow = require("./src/stopFlow");
 
 
 
 (async () => {
-  // const tx = await startFlow("0xdA064B1Cef52e19caFF22ae2Cc1A4e8873B8bAB0");
-  // const tx = await stopFlow("0xdA064B1Cef52e19caFF22ae2Cc1A4e8873B8bAB0");
+  // await startFlow("0xdA064B1Cef52e19caFF22ae2Cc1A4e8873B8bAB0");
+  // await stopFlow("0xdA064B1Cef52e19caFF22ae2Cc1A4e8873B8bAB0");
 
   const details = await getFlow();
   console.log(details)
